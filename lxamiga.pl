1e220ef5774fe486ca46adc3c5913c4ccc5d2b17
Guenter Bartsch <guenter@zamia.org>
2019-09-01 23:55:08 +0200

    add re-sync support, baudrate option

--- lxamiga.pl
+++ lxamiga.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl -w
 #
 # Simple Linux client for CloanTo Amiga Explorer running on a Commodore Amiga
-# Version 1.01 - 24/02/2019
+# Version 2.00 - 01/09/2019
 #
 # by Mark Street <marksmanuk@gmail.com>
+#
+# re-sync support, baudrate option added by G. Bartsch
 #
 # Example usage:
 # 	lxamiga -l
@@ -25,17 +27,18 @@ use 5.010;
 
 my $serial;
 my $socket;
-my $serport 	= "/dev/ttyUSB0";
-my $serbaud		= 19200;
-my $iphost		= "192.168.1.200";
-my $ipport		= "356";
+my $serport     = "/dev/ttyUSB0";
+my $serbaud	    = 19200;
+my $iphost	    = "192.168.1.200";
+my $ipport	    = "356";
 my $password	= "AExplorer";
-my $maxlen		= 512;
+my $maxlen	    = 512;
 my $sequence 	= 1;
 
 my %args;
 my $usage = "lxamiga by Mark Street <marksmanuk\@gmail.com>\n\nUsage: lxamiga [options]\n".
 	"\t-t Use TCP/IP lan connection (dflt. serial)\n".
+	"\t-b <baudrate> Set baudrate (dflt: 19200)\n".
 	"\t-l List available devices\n".
 	"\t-d <volume:path> dir\n".
 	"\t-r read file <device:volume/path>\n".
@@ -44,16 +47,21 @@ my $usage = "lxamiga by Mark Street <marksmanuk\@gmail.com>\n\nUsage: lxamiga [o
 	"\t-f <device> Name format disk\n".
 	"\t-w <file> write output to filename\n".
 	"\t-v Verbose\n";
-getopts("tld:r:s:u:f:w:v", \%args) || die $usage;
+getopts("tld:r:s:u:f:w:vb:", \%args) || die $usage;
+
+if ($args{b})
+{
+    $serbaud = $args{b};
+}
 
 initialise();
 get_connect();
-get_devices()					if $args{l};
-get_directory($args{d})			if $args{d};
-get_file($args{r}, $ARGV[0])	if $args{r};
-put_file($args{s}, $ARGV[0])	if $args{s};
-del_file($args{u})				if $args{u};
-format_disk($args{f})			if $args{f};
+get_devices()                   if $args{l};
+get_directory($args{d})	        if $args{d};
+get_file($args{r}, $ARGV[0])    if $args{r};
+put_file($args{s}, $ARGV[0])    if $args{s};
+del_file($args{u})              if $args{u};
+format_disk($args{f})           if $args{f};
 finalise();
 
 sub Dump
@@ -114,11 +122,16 @@ sub crc32
 
 	my $crc = $init_value ^ 0xffffffff;
 
+    #printf "crc32... : crc = 0x%08x\n", $crc;
+    my $myi = 0;
 	foreach my $x (unpack ('C*', $input)) {
    		$crc = (($crc >> 8) & 0xffffff) ^ $lookup_table[ ($crc ^ $x) & 0xff ];
+                #printf "crc32: myi=%d x=%02x, crc=%08x\n", $myi, $x, $crc;
+                $myi = $myi + 1; 
 	}
 
 	$crc = $crc ^ 0xffffffff;
+        #printf "crc32... done: crc = 0x%08x\n", $crc;
 	return $crc;
 }
 
@@ -224,7 +237,7 @@ sub write_port
 
 	print "="x73,"\n" if $args{v};
 	print "SENDING ".(length($msg))." BYTES:\n" if $args{v};
-	Dump($msg) if $args{v};
+	# Dump($msg) if $args{v};
 
 	write_serial($msg) if defined $serial;
 	write_socket($msg) if defined $socket;
@@ -242,97 +255,96 @@ sub write_socket
 	$socket->send($msg);
 }
 
+sub write_ack
+{
+	write_serial("PkOk") if defined $serial;
+	write_socket("PkOk") if defined $socket;
+}
+
+sub write_not_ack
+{
+	write_serial("PkRs") if defined $serial;
+	write_socket("PkRs") if defined $socket;
+}
+
 sub read_message
 {
 	my $rx;
-	for (my $i=0; $i<10; $i++)
-	{
-		$rx = read_port(12);			# Header
-		last if length($rx);
-		sleep(1);
-	}
+    my $head;
+	my $head_ok    = 1;
+	my $payload_ok = 1;
 
-	die "Message timeout error" unless length($rx);
-	my $head = decode_header($rx);
+    do {			         				# repeat until CRC is ok
 
-	if ($head->{len})
-	{
-		$rx = read_port($head->{len});	# Payload
-		my $crc = read_port(4);			# CRC32 for payload
-		$crc = unpack("N", $crc);
+        # header
 
-		my $calc_crc = crc32($rx);
-		printf "  CRC32 = %04X (%04X)\n", $crc, $calc_crc if $args{v};
+		for (my $i=0; $i<10; $i++)
+		{
+			$rx = read_port(12);			# Header
+			last if length($rx);
+			sleep(1);
+		}
+		die "Message timeout error" unless length($rx);
 
-		printf "Mismatched CRC on payload (%04X/%04X)\n", $crc, $calc_crc
-			if $crc != $calc_crc;
-	}
+		# decode header
+
+		my ($id, $len, $seq, $head_crc) = unpack("nnNN", $rx);
+		my $calc_crc = crc32(substr($rx, 0, 12-4));
+
+		if ($args{v})
+		{
+			printf "  Message  = %02X\n", $id;
+			printf "  Length   = %d\n", $len;
+			printf "  Sequence = %d\n", $seq;
+			printf "  CRC32    = %04X (%04X)\n", $head_crc, $calc_crc;
+		}
+
+		my $head_ok = $head_crc == $calc_crc;
+
+		printf "Mismatched CRC on header (%04X/%04X) message $id\n", $head_crc, $calc_crc
+			if (!$head_ok);
+
+		$head = {
+			    id	 	=> $id,
+			    len		=> $len,
+			    sequence 	=> $seq,
+			   };
+
+		if ( $head_ok && $head->{len} )
+		{
+			$rx = read_port($head->{len});	# Payload
+			my $crc = read_port(4);			# CRC32 for payload
+			$crc = unpack("N", $crc);
+
+			my $calc_crc = crc32($rx);
+			printf "  CRC32 = %04X (%04X)\n", $crc, $calc_crc if $args{v};
+
+			$payload_ok = $crc == $calc_crc;
+
+			printf "Mismatched CRC on payload (%04X/%04X)\n", $crc, $calc_crc
+				if (!$payload_ok);
+		}
+
+
+		if ( !$head_ok || !$payload_ok )
+        {
+            # read pending bytes (there might have been payload)
+            do
+            {
+                $rx = read_port($maxlen);
+			} until (length($rx) eq 0);
+
+		    write_not_ack();
+        }
+
+	} until $head_ok && $payload_ok ;
+
+	write_ack();
 
 	return 
 	{
 		header  => $head,
 		payload => $rx,
-	}
-}
-
-sub write_message
-{
-	my $id 		= shift;
-	my $payload = shift;
-
-	my $msg = pack("n", $id);
-	$msg .= pack("n", length($payload));
-	$msg .= pack("N", $sequence++);	
-
-	my $crc = crc32($msg);
-	$msg .= pack("N", $crc);
-
-	if ($args{v})
-	{
-		print "="x73,"\n";
-		print "WRITE MESSAGE:\n";
-		printf "  Message    = %04X\n", $id;
-		printf "  Length     = %d\n", length($payload);
-		printf "  Sequence   = %d\n", $sequence-1;
-		printf "  Header CRC = %08X\n", $crc;
-	}
-
-	if (length($payload))
-	{
-		$msg .= $payload;
-		$crc = crc32($payload);
-		$msg .= pack("N", $crc);
-		Dump($payload, "WRITE PAYLOAD") if $args{v};
-	}
-
-	write_port($msg);
-}
-
-sub decode_header
-{
-	my $rx = shift || die "Undefined message";
-	die "invalid header length (".(length($rx)).") expect 12 bytes"
-		if length($rx) != 12;
-
-	my ($id, $len, $seq, $crc) = unpack("nnNN", $rx);
-	my $calc_crc = crc32(substr($rx, 0, 12-4));
-
-	if ($args{v})
-	{
-		printf "  Message  = %04X\n", $id;
-		printf "  Length   = %d\n", $len;
-		printf "  Sequence = %d\n", $seq;
-		printf "  CRC32    = %04X (%04X)\n", $crc, $calc_crc;
-	}
-
-	printf "Mismatched CRC on header (%04X/%04X) message $id\n", $crc, $calc_crc
-		if $crc != $calc_crc;
-
-	return 
-	{
-		id		 	=> $id,
-		len			=> $len,
-		sequence 	=> $seq,
 	}
 }
 
@@ -346,10 +358,49 @@ sub read_ack
 	}
 }
 
-sub write_ack
+sub write_message
 {
-	write_serial("PkOk") if defined $serial;
-	write_socket("PkOk") if defined $socket;
+	my $id 	    = shift;
+	my $payload = shift;
+    my $ack;
+
+	do
+    {
+
+		my $msg = pack("n", $id);
+		$msg .= pack("n", length($payload));
+		$msg .= pack("N", $sequence++);	
+
+		my $crc = crc32($msg);
+		$msg .= pack("N", $crc);
+
+		if ($args{v})
+		{
+			print "="x73,"\n";
+			print "WRITE MESSAGE:\n";
+			printf "  Message    = %04X\n", $id;
+			printf "  Length     = %d\n", length($payload);
+			printf "  Sequence   = %d\n", $sequence-1;
+			printf "  Header CRC = %08X\n", $crc;
+		}
+
+		if (length($payload))
+		{
+			$msg .= $payload;
+			$crc = crc32($payload);
+			$msg .= pack("N", $crc);
+			# Dump($payload, "WRITE PAYLOAD") if $args{v};
+		}
+
+		write_port($msg);
+		
+		$ack = read_ack();
+        
+		if ($ack ne "PkOk")
+		{
+            print "NACK $ack received -> resync.";
+        }
+	} until $ack eq "PkOk";
 }
 
 sub get_connect
@@ -366,22 +417,9 @@ sub get_connect
 
 	# Send 0x0002 message and wait 0x0002 response:
 	write_message(2, $initmsg);
-	my $ack = read_ack();
-
-	# Host may be waiting for acknowledgement from us if we're out of sequence:
-	if (length($ack) == 4 && $ack !~ /PkOk/)
-	{
-		write_ack();
-		get_connect();
-		return;
-	}
-
-	die "No response from Amiga.  Is AExplorer running?" unless length($ack);
-	die "Unexpected compliance ($ack) for 0x002" unless $ack eq "PkOk";
 
 	my $rx = read_message();
 	die "Unexpected message received ($rx)" unless $rx->{payload} =~ /Cloanto/;
-	write_ack();
 
 	print "Connected to host successfully at $serbaud baud.\n" if defined $serial;
 	print "Connected to $iphost:$ipport successfully.\n" if defined $socket;
@@ -394,7 +432,6 @@ sub get_multipart
 	# final 0x0004.
 
 	my $rx = read_message();	# Should be 0x0003
-	write_ack();
 	die "Invalid request ($rx->{header}{id})" if $rx->{header}{id} != 0x0003;
 
 	my $total = unpack "N", $rx->{payload};
@@ -404,8 +441,6 @@ sub get_multipart
 	do
 	{
 		write_message(0x0000, "");	# Send 0x0000 to solicit reply
-		my $ack = read_ack();
-		die "Unexpected compliance ($ack) for 0x0000" unless $ack eq "PkOk";
 
 		$rx = read_message();
 		if ($rx->{header}{len})
@@ -418,7 +453,6 @@ sub get_multipart
 				length($msg), $total, (length($msg)/$total)*100;
 			flush STDOUT;
 		}
-		write_ack();
 	}
 	while ($rx->{header}{id} == 0x0005);	# Received last part
 
@@ -442,13 +476,10 @@ sub put_multipart
 
 	# Wait for first 0x0000
 	my $rx = read_message();
-	write_ack();
 
 	if ($rx->{header}{id} != 0x0000)
 	{
 		write_message(0x0000, "");
-		my $ack = read_ack();
-		die "Unexpected compliance ($ack) for 0x0000" unless $ack eq "PkOk";
 		die "File exists ($rx->{header}{id})"
 			if !$overwrite && $rx->{header}{id} == 0x0008;
 	}
@@ -456,8 +487,6 @@ sub put_multipart
 	# Send 0x0003 prefix:
 	my $payload = pack "NN", $len, 0x00000200;
 	write_message(0x0003, $payload);
-	my $ack = read_ack();
-	die "Unexpected compliance ($ack) for 0x0003" unless $ack eq "PkOk";
 
 	# Send 0x0005 parts:
 	my $buffer = $data;
@@ -466,7 +495,6 @@ sub put_multipart
 	{
 		# Wait for host to solicit reply with 0x0000:
 		$rx = read_message();
-		write_ack();
 		die "Invalid request ($rx->{header}{id})" if $rx->{header}{id} != 0x0000;
 
 		# Send next part:
@@ -477,8 +505,6 @@ sub put_multipart
 		$tx .= substr($buffer, 0, $num_tx);
 		write_message(0x0005, $tx);
 
-		my $ack = read_ack();
-		die "Unexpected compliance ($ack) for 0x0005" unless $ack eq "PkOk";
 		$buffer = substr($buffer, $num_tx);
 		$sent += $num_tx;
 
@@ -490,25 +516,19 @@ sub put_multipart
 
 	# Wait for host to solicit reply with 0x0000:
 	$rx = read_message();
-	write_ack();
 	die "Invalid request ($rx->{header}{id})" if $rx->{header}{id} != 0x0000;
 
 	# Send final 0x0004:
 	write_message(0x0004, "");
-	$ack = read_ack();
-	die "Unexpected compliance ($ack) for 0x0004" unless $ack eq "PkOk";
 }
 
 sub send_close
 {
 	# Send 0x006D and await 0x000A response:
 	write_message(0x006D, "");
-	my $ack = read_ack();
-	die "Unexpected compliance ($ack) for 0x006D" unless $ack eq "PkOk";
 
 	my $rx = read_message();
 	die "Expected 0x000A reply ($rx->{header}{id})" if $rx->{header}{id} != 0x000A;
-	write_ack();
 }
 
 sub get_devices
@@ -522,8 +542,6 @@ sub get_directory
 
 	# Send 0x0064 message and wait 0x0003 response:
 	write_message(0x0064, $device."\x00\x01");
-	my $ack = read_ack();
-	die "Unexpected compliance ($ack) for 0x0064" unless $ack eq "PkOk";
 
 	my $msg_devices = get_multipart();
 	send_close();
@@ -627,8 +645,6 @@ sub get_file
 
 	# Send 0x0065 message and wait 0x0003 response:
 	write_message(0x0065, $device."\x00");
-	my $ack = read_ack();
-	die "Unexpected compliance ($ack) for 0x0064" unless $ack eq "PkOk";
 
 	my $rx = get_multipart;
 	send_close();
@@ -690,8 +706,6 @@ sub put_file
 	
 	# Send 0x0066 message and wait 0x0000 response:
 	write_message(0x0066, $msg);
-	my $ack = read_ack();
-	die "Unexpected compliance ($ack) for 0x0066" unless $ack eq "PkOk";
 
 	# Send file:
 	my $data;
@@ -713,11 +727,8 @@ sub del_file
 	my $file = shift || die "Unspecified file";
 	
 	write_message(0x0067, $file."\x00");
-	my $ack = read_ack();
-	die "Unexpected compliance ($ack) for 0x0067" unless $ack eq "PkOk";
 
 	my $rx = read_message();
-	write_ack();
 	die "Invalid request ($rx->{header}{id})" if $rx->{header}{id} != 0x0000;
 
 	send_close();
@@ -735,12 +746,9 @@ sub format_disk
 	my $opts = 0x00;	# TODO support filesytem and format options
 	my $msg = "DOS\1".(pack "N", $opts)."$device\0$name\0EXE";
 	write_message(0x006e, $msg);
-	my $ack = read_ack();
-	die "Unexpected compliance ($ack) for 0x006e" unless $ack eq "PkOk";
 
 	# Read reply from host (ignore any warnings):
 	my $rx = read_message();
-	write_ack();
 	if ($rx->{header}{id} != 0x0000 && $rx->{header}{id} != 0x0008)
 	{
 		send_close();	# Abort
@@ -754,13 +762,10 @@ sub format_disk
 	if ($rx->{header}{id} == 0x0008) 	# Write protect
 	{
 		write_message(0x0000, "");	# Continue
-		my $ack = read_ack();
-		die "Format failed to force overwrite ($ack)" unless $ack eq "PkOk";
 	}
 	
 	# Read 0x000B from host:
 	$rx = read_message();
-	write_ack();
 	die "Unexpected message received ($rx->{header}{id})" if $rx->{header}{id} != 0x000B;
 
 	my $total = unpack "N", $rx->{payload};		# Number of tracks + 1
@@ -774,12 +779,10 @@ sub format_disk
 		flush STDOUT;
 
 		$rx = read_message();
-		write_ack();
 		die "Unexpected message received ($rx->{header}{id})" if $rx->{header}{id} != 0x0000;
 	
 		# Send 0x0000 to host:
 		write_message(0x0000, "");
-		my $ack = read_ack();
 	}
 
 	print "\nFinished.\n";
